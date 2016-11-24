80cfb7ea9c8a01a192cd8c45d1083c894417a9a9
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-11-24 08:18:07 +0000

    updated

--- echo.py
+++ echo.py
@@ -5,7 +5,8 @@ def main():
     data = sys.stdin.readlines()
     # this is a json string in one line so we are interested in that one line
     payload = data[0]
-    print(payload['body'])
+    response = {'body': payload['body'], 'status': 201}
+    print(response)
 
 if __name__ == "__main__":
     main()
