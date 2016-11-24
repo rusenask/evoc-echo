35195dd6e19c4659b0410c80aaca2457c96271d9
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-11-24 08:42:44 +0000

    json

--- echo.py
+++ echo.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 import sys
+import json
+
+# response - creates json structure
+def response(body, status):
+    resp = {
+        'status': status,
+        'body': body,
+        }
+    # converting to json        
+    print(json.dumps(resp))    
+
 
 def main():
     data = sys.stdin.readlines()
     # this is a json string in one line so we are interested in that one line
     payload = data[0]
-    response = {'body': payload['body'], 'status': 201}
-    print(response)
+    
+    request = json.loads(payload)
+    
+    response('%s' % (request['body']), 201)
 
 if __name__ == "__main__":
     main()
